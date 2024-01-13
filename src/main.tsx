c7c26a62d94eab34e07db9e3c76519a0c665caa4
Moiz Amjad <moiz.amjad37@gmail.com>
2024-01-13 16:10:34 -0500

    navbar + mobile vew navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './main.css'
 import './navigationBar/navbarStyles.css'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
--- src/navigationBar/navbar.tsx
+++ src/navigationBar/navbar.tsx
@@ -1,20 +1,42 @@
 
 export default function navbar() {
+  
+  function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".menu-icon");
+    menu?.classList.toggle("open");
+    icon?.classList.toggle("open");
+  }
+
   return (
     <div className="nav-container">
         
-        <div>
-          Moiz Amjad
-        </div>
+        <div className="logo"> Moiz Amjad </div>
 
-        <div>
-          
+        <div> 
           <ul className="nav-links">
             <li><a href="#about"> About </a></li>
             <li><a href="#experience"> Experience </a></li>
             <li><a href="#projects"> Projects </a></li>
             <li><a href="#contact"> Contact </a></li>
-          </ul>
+          </ul> 
+        </div>
+
+        <div className="mobile-nav">
+          <div className="nav-menu">
+            <div className="menu-icon" onClick={toggleMenu}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+
+            <div className="menu-links">  
+              <li><a href="#about" onClick={toggleMenu}> About </a></li>
+              <li><a href="#experience" onClick={toggleMenu}> Experience </a></li>
+              <li><a href="#projects" onClick={toggleMenu}> Projects </a></li>
+              <li><a href="#contact" onClick={toggleMenu}> Contact </a></li>
+            </div>
+          </div>
         </div>
 
     </div>
