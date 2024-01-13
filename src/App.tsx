dd3c2289b28365c1f9664d6622b22141024ed97b
Moiz Amjad <moiz.amjad37@gmail.com>
2024-01-12 23:14:35 -0500

    navbar (in progress)-1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Navbar from './navigationBar/navbar';
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/navigationBar/navbar.tsx
@@ -0,0 +1,22 @@
+
+export default function navbar() {
+  return (
+    <div className="nav-container">
+        
+        <div>
+          Moiz Amjad
+        </div>
+
+        <div>
+          
+          <ul className="nav-links">
+            <li><a href="#about"> About </a></li>
+            <li><a href="#experience"> Experience </a></li>
+            <li><a href="#projects"> Projects </a></li>
+            <li><a href="#contact"> Contact </a></li>
+          </ul>
+        </div>
+
+    </div>
+  )
+}
